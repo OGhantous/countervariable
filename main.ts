15b76cb010f947a40ffc83f5178def680cceca4c
OGhantous <122539542+OGhantous@users.noreply.github.com>
2023-01-26 08:45:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 let resetNumber = 0
-
 input.onButtonPressed(Button.A, function () {
     resetNumber += 1
+    basic.showNumber(resetNumber)
 })
 input.onButtonPressed(Button.AB, function () {
     resetNumber = 0
+    basic.showNumber(resetNumber)
 })
 input.onButtonPressed(Button.B, function () {
     resetNumber += -1
-})
-basic.forever(function () {
     basic.showNumber(resetNumber)
 })
